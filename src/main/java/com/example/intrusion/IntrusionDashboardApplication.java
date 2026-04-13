70745cbf7e3c35b128891cdb00cb3e7adc2c2021
younez999 <139119206+younez999@users.noreply.github.com>
2026-04-13 09:42:44 +0800

    creating the IntrusionDashboardApplication

---
+++ src/main/java/com/example/intrusion/IntrusionDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.intrusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrusionDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntrusionDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/intrusion/IntrusionDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intrusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrusionDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
